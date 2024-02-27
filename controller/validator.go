98ccbf2fdf33aa272ff7ab85a388d225d40eef28
Github Doodshen <Github 2475169766@qq.com>
2024-02-27 21:16:24 +0800

    去除结构体名称前缀的自定义方法

--- controller/validator.go
+++ controller/validator.go
@@ -1,7 +1,17 @@
+/*
+ * @Author: Github Doodshen Github 2475169766@qq.com
+ * @Date: 2024-02-27 20:31:01
+ * @LastEditors: Github Doodshen Github 2475169766@qq.com
+ * @LastEditTime: 2024-02-27 21:15:20
+ * @FilePath: \2024.2.3 bluebell\controller\validator.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 package controller
 
 import (
 	"fmt"
+	"reflect"
+	"strings"
 
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/locales/en"
@@ -14,10 +24,21 @@ import (
 
 //翻译校验错误提示信息
 
+var trans ut.Translator
+
 // InitTrans 初始化翻译器
 func InitTrans(locale string) (err error) {
 	// 修改gin框架中的Validator引擎属性，实现自定制
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+
+		//注册一个获取json tag的自定义方法
+		v.RegisterTagNameFunc(func(fld reflect.StructField) string {
+			name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+			if name == "-" {
+				return ""
+			}
+			return name
+		})
 
 		zhT := zh.New() // 中文翻译器
 		enT := en.New() // 英文翻译器
@@ -30,7 +51,7 @@ func InitTrans(locale string) (err error) {
 		// locale 通常取决于 http 请求头的 'Accept-Language'
 		var ok bool
 		// 也可以使用 uni.FindTranslator(...) 传入多个locale进行查找
-		trans, ok := uni.GetTranslator(locale)
+		trans, ok = uni.GetTranslator(locale)
 		if !ok {
 			return fmt.Errorf("uni.GetTranslator(%s) failed", locale)
 		}
@@ -47,4 +68,13 @@ func InitTrans(locale string) (err error) {
 		return
 	}
 	return
+}
+
+// 去除结构体名称前缀的自定义方法
+func removeTopStruct(fields map[string]string) map[string]string {
+	res := map[string]string{}
+	for field, err := range fields {
+		res[field[strings.Index(field, ".")+1:]] = err
+	}
+	return res
 }
