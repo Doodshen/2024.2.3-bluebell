971791023103da686934bcf3dede077448e0f393
Github Doodshen <Github 2475169766@qq.com>
2024-02-27 22:05:13 +0800

    更新必须匹配的参数字段

--- models/params.go
+++ models/params.go
@@ -4,5 +4,5 @@ package models
 type ParamSignUp struct {
 	UserName   string `json:"username" binding:"required"`
 	Password   string `json:"password" binding:"required"`
-	RePassword string `json:"repassword" binding:"required,eqfield=Password"`
+	RePassword string `json:"re_password" binding:"required,eqfield=Password"`
 }
