bb29d90e1d9774fe6d4559d4e15ac1c6a4335bd7
Github Doodshen <Github 2475169766@qq.com>
2024-02-27 21:15:05 +0800

    增加获取参数以及参数校验

--- controller/user.go
+++ controller/user.go
@@ -1,25 +1,42 @@
-/*
- * @Author: Github Doodshen Github 2475169766@qq.com
- * @Date: 2024-02-25 12:30:44
- * @LastEditors: Github Doodshen Github 2475169766@qq.com
- * @LastEditTime: 2024-02-25 12:44:43
- * @FilePath: \2024.2.3 bluebell\controller\user.go
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 package controller
 
 import (
 	"net/http"
 	"web_app/logic"
+	"web_app/models"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.uber.org/zap"
 )
 
 // signupHandler 处理注册请求的函数
 func SignUpHandler(c *gin.Context) {
-	//1 获取参数和参数校验 ---controller层处理
-	//2 业务处理          ---logic层
-	logic.SignUp()
+
+	//1 获取参数和参数校验
+	p := new(models.ParamSignUp)
+	if err := c.ShouldBind(p); err != nil {
+
+		zap.L().Error("SingUp with invalid param ", zap.Error(err))
+
+		//判断err是不是validator.validationErrors类型
+		errs, ok := err.(validator.ValidationErrors)
+		if !ok {
+			c.JSON(http.StatusOK, gin.H{
+				"msg": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"msg": removeTopStruct(errs.Translate(trans)),
+		})
+		return
+	}
+
+	//2 业务处理
+	logic.SignUp(p)
 	//3 返回响应
-	c.JSON(http.StatusOK, "ok")
+	c.JSON(http.StatusOK, gin.H{
+		"msg": "success",
+	})
 }
