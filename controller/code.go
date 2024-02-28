5e3c33a67226c5de1388ae957269e1b0928db842
Github Doodshen <Github 2475169766@qq.com>
2024-02-28 14:33:38 +0800

    增加错误状态码以及根据状态码返回错误信息方法

--- controller/code.go
+++ controller/code.go
@@ -1,20 +1,31 @@
 package controller
 
-
-import ()
-
-//定义错误码类型 
+//定义错误码类型
 type Rescode int64
 
-
 //定义错误码
-const(
-	CodeSuccess Rescode = 1000+iota
-	CodeInvalidParam 
+const (
+	CodeSuccess Rescode = 1000 + iota
+	CodeInvalidParam
 	CodeUserExist
 	CodeUserNotExist
 	CodeInvalidPassword
 	CodeServerBusy
 )
 
-var codeMasMap = map[]
+var codeMasMap = map[Rescode]string{
+	CodeSuccess:         "Success",
+	CodeInvalidParam:    "参数错误",
+	CodeUserExist:       "用户已经存在",
+	CodeInvalidPassword: "密码错误",
+	CodeServerBusy:      "服务器繁忙",
+}
+
+//Msg()根据状态码返回错误信息
+func (c Rescode) Msg() string {
+	msg, ok := codeMasMap[c]
+	if !ok {
+		msg = codeMasMap[CodeServerBusy]
+	}
+	return msg
+}
