0b3ed90c03d15035b7d0924fa88182b5dbc08c31
Github Doodshen <Github 2475169766@qq.com>
2024-02-28 14:34:32 +0800

    增加ParamLogin保存登录参数

--- models/params.go
+++ models/params.go
@@ -1,3 +1,11 @@
+/*
+ * @Author: Github Doodshen Github 2475169766@qq.com
+ * @Date: 2024-02-27 20:11:23
+ * @LastEditors: Github Doodshen Github 2475169766@qq.com
+ * @LastEditTime: 2024-02-28 13:42:00
+ * @FilePath: \2024.2.3 bluebell\models\params.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 package models
 
 //ParamSignUp注册参数
