c1606a287684c7da7a3a958874e24cc7db73fe72
Github Doodshen <Github 2475169766@qq.com>
2024-02-28 13:42:00 +0800

    增加登录对接数据库结构体

--- models/params.go
+++ models/params.go
@@ -1,8 +1,14 @@
 package models
 
-//定义请求参数的结构体
+//ParamSignUp注册参数
 type ParamSignUp struct {
 	UserName   string `json:"username" binding:"required"`
 	Password   string `json:"password" binding:"required"`
 	RePassword string `json:"re_password" binding:"required,eqfield=Password"`
+}
+
+//ParamLogin登录参数
+type ParamLogin struct {
+	UserName string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
