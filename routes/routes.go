08316af06aac85171cc9300c015eedb6cc4ba667
Github Doodshen <Github 2475169766@qq.com>
2024-02-28 11:17:09 +0800

    增加根据配置文件mode来配置gin框架模式

--- routes/routes.go
+++ routes/routes.go
@@ -2,7 +2,7 @@
  * @Author: Github Doodshen Github 2475169766@qq.com
  * @Date: 2024-01-30 21:03:01
  * @LastEditors: Github Doodshen Github 2475169766@qq.com
- * @LastEditTime: 2024-02-19 20:59:32
+ * @LastEditTime: 2024-02-28 11:12:00
  * @FilePath: \web-app2\routes\routes.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -17,7 +17,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SetUpRouter() *gin.Engine {
+func SetUpRouter(mode string) *gin.Engine {
+	//判断项目模式
+	if mode == gin.ReleaseMode {
+		gin.SetMode(gin.ReleaseMode) //gin设置为发布模式
+	}
 
 	r := gin.New()
 	r.Use(logger.GinLogger(), logger.GinRecovery(true))
