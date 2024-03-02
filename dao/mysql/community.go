0f51df92af2a9d38e2f234d8bb7b8083d83bb747
Github Doodshen <Github 2475169766@qq.com>
2024-03-02 16:29:06 +0800

    增加返回列表接口功能

--- dao/mysql/community.go
+++ dao/mysql/community.go
@@ -2,7 +2,7 @@
  * @Author: Github Doodshen Github 2475169766@qq.com
  * @Date: 2024-03-02 15:08:49
  * @LastEditors: Github Doodshen Github 2475169766@qq.com
- * @LastEditTime: 2024-03-02 16:18:06
+ * @LastEditTime: 2024-03-02 16:28:29
  * @FilePath: \2024.2.3 bluebell\dao\mysql\community.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
